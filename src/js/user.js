a8d1ef9c5d1b85ecc4659515d3b0536c824b07da
YY <z3388638@gmail.com>
2017-05-24 23:20:09 +0800

    store user info; update submodule

--- src/js/user.js
+++ src/js/user.js
@@ -1,5 +1,10 @@
 var User = (function () {
-	
+	var _userInfo;
+
+	function _setUserInfo(userInfo) {
+		_userInfo = userInfo;
+	}
+
 	function login(loginForm) {
 		return fetch('https://api.overseas.ncnu.edu.tw/users/login', {
 			method: "POST",
@@ -23,10 +28,37 @@ var User = (function () {
 			credentials: 'include'
 		});
 	}
+	
+	// permission: 'school_editor' | 'school_reviewer'
+	function checkLogin(permission) {
+		isLogin().then(function (res) {
+			if(res.ok) {
+				return res.json();
+			} else {
+				throw res.status;
+			}
+		}).then(function (json) {
+			if (!json[permission] || json[permission].has_banned) {
+				location.replace('/school/login.html');
+			} else {
+				_setUserInfo(json);
+			}
+		}).catch(function (err) {
+			if (err == 401) {
+				location.replace('/school/login.html');
+			}
+		});
+	}
+
+	function getUserInfo() {
+		return _userInfo
+	}
 
 	return {
 		login, 
 		logout, 
-		isLogin
+		isLogin, 
+		checkLogin, 
+		getUserInfo
 	}
 })();
