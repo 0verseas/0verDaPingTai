6df0571e63fc5d205274a81624b24b407a94a805
lemon5920 <lemon5920@gmail.com>
2017-06-25 22:26:04 +0800

    Check Browser support.

---
+++ src/js/browser.js
@@ -0,0 +1,52 @@
+var Browser = (function(){
+
+	var _browserInfo = {};
+	
+	function _setInfo() {
+
+		/*
+		* return an object, ex:
+		* {
+		*   name: "Chrome",
+		*   version: 58
+		* }
+		*/
+
+		var ua = navigator.userAgent, tem,
+		M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+		if (/trident/i.test(M[1])) {
+			tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+			_browserInfo = { name: 'IE', version: (tem[1] || '') };
+		}
+		if (M[1] === 'Chrome') {
+			tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+			if ( tem!= null ) {
+				_browserInfo = { name: tem.slice(1)[0], version: tem.slice(1)[1] };
+			};
+		}
+		M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+		if ((tem= ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+		_browserInfo = {
+			name: M[0],
+			version: Number(M[1])
+		}
+	}
+
+	function getInfo() {
+		_setInfo();
+		return _browserInfo;
+	}
+
+	function checkBrowserSupport() {
+		// true: 可支援的瀏覽器
+		// false: 不支援或版本不夠新的瀏覽器
+		var info = getInfo();
+		return !(info.name === 'IE' || (info.name === 'Edge' && info.version < 15));
+	}
+
+	return {
+		getInfo,
+		checkBrowserSupport
+	}
+	
+})();
